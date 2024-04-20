b84f96743ded500e4d5c2fdf0e1e7adc3d865457
Daniel Menna <danielribeiromenna@gmail.com>
2024-04-20 16:52:01 -0300

    arquivo schema finalizado

--- src/backend/schema.py
+++ src/backend/schema.py
@@ -13,6 +13,18 @@ class ProductBase(BaseModel) :
 class ProductCreate(ProductBase) :
     pass
 
+class ProductUpdate(ProductBase) :
+    name : Optional[str] = None
+    description : Optional[str] = None
+    prince : Optional[PositiveFloat] = None
+    category : Optional[str] = None
+    vendor_email: Optional[EmailStr] = None
 
+class ProductResponse(ProductBase) :
+    id: int
+    created_at: datetime
+
+    class config :
+        from_atributtes = True
 
 
