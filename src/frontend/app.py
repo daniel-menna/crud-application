2463d40766b333ab9bff9028954dd2e71e15ee92
Daniel Menna <danielribeiromenna@gmail.com>
2024-04-21 10:53:34 -0300

    Project Finished!

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -6,40 +6,40 @@ st.set_page_config(layout="wide")
 
 st.image("logo.png", width=200)
 
-st.title("Gerenciamento de Produtos")
+st.title("Product Management")
 
 
-# Função auxiliar para exibir mensagens de erro detalhadas
+# Auxiliary function to get more details about errors
 def show_response_message(response):
     if response.status_code == 200:
-        st.success("Operação realizada com sucesso!")
+        st.success("Succes! Operation completed.")
     else:
         try:
             data = response.json()
             if "detail" in data:
-                # Se o erro for uma lista, extraia as mensagens de cada erro
+                # If the error is a list, extract each error details
                 if isinstance(data["detail"], list):
                     errors = "\n".join([error["msg"] for error in data["detail"]])
-                    st.error(f"Erro: {errors}")
+                    st.error(f"Error: {errors}")
                 else:
                     # Caso contrário, mostre a mensagem de erro diretamente
-                    st.error(f"Erro: {data['detail']}")
+                    st.error(f"Error: {data['detail']}")
         except ValueError:
-            st.error("Erro desconhecido. Não foi possível decodificar a resposta.")
+            st.error("Unknown error. It was not possible to transcribe the message.")
 
 
 # Adicionar Produto
-with st.expander("Adicionar um Novo Produto"):
+with st.expander("Add New Product"):
     with st.form("new_product"):
-        name = st.text_input("Nome do Produto")
-        description = st.text_area("Descrição do Produto")
-        price = st.number_input("Preço", min_value=0.01, format="%f")
-        categoria = st.selectbox(
-            "Categoria",
-            ["Eletrônico", "Eletrodoméstico", "Móveis", "Roupas", "Calçados"],
+        name = st.text_input("Product Name")
+        description = st.text_area("Product Description")
+        price = st.number_input("Price", min_value=0.01, format="%f")
+        category = st.selectbox(
+            "Category",
+            ["Eletronic", "Eletrodomestic", "Forniture", "Clothes", "Shoes"],
         )
-        email_fornecedor = st.text_input("Email do Fornecedor")
-        submit_button = st.form_submit_button("Adicionar Produto")
+        vendor_email = st.text_input("Vendor e-mail")
+        submit_button = st.form_submit_button("Add Product")
 
         if submit_button:
             response = requests.post(
@@ -48,14 +48,14 @@ with st.expander("Adicionar um Novo Produto"):
                     "name": name,
                     "description": description,
                     "price": price,
-                    "categoria": categoria,
-                    "email_fornecedor": email_fornecedor,
+                    "category": category,
+                    "vendor_email": vendor_email
                 },
             )
             show_response_message(response)
 # Visualizar Produtos
-with st.expander("Visualizar Produtos"):
-    if st.button("Exibir Todos os Produtos"):
+with st.expander("Show Product"):
+    if st.button("Show all products"):
         response = requests.get("http://backend:8000/products/")
         if response.status_code == 200:
             product = response.json()
@@ -67,8 +67,8 @@ with st.expander("Visualizar Produtos"):
                     "name",
                     "description",
                     "price",
-                    "categoria",
-                    "email_fornecedor",
+                    "category",
+                    "vendor_email",
                     "created_at",
                 ]
             ]
@@ -79,9 +79,9 @@ with st.expander("Visualizar Produtos"):
             show_response_message(response)
 
 # Obter Detalhes de um Produto
-with st.expander("Obter Detalhes de um Produto"):
-    get_id = st.number_input("ID do Produto", min_value=1, format="%d")
-    if st.button("Buscar Produto"):
+with st.expander("Get Product Details"):
+    get_id = st.number_input("Product ID", min_value=1, format="%d")
+    if st.button("Search Product"):
         response = requests.get(f"http://backend:8000/products/{get_id}")
         if response.status_code == 200:
             product = response.json()
@@ -93,8 +93,8 @@ with st.expander("Obter Detalhes de um Produto"):
                     "name",
                     "description",
                     "price",
-                    "categoria",
-                    "email_fornecedor",
+                    "category",
+                    "vendor_email",
                     "created_at",
                 ]
             ]
@@ -105,30 +105,30 @@ with st.expander("Obter Detalhes de um Produto"):
             show_response_message(response)
 
 # Deletar Produto
-with st.expander("Deletar Produto"):
-    delete_id = st.number_input("ID do Produto para Deletar", min_value=1, format="%d")
-    if st.button("Deletar Produto"):
+with st.expander("Delete Product"):
+    delete_id = st.number_input("The ID of Product to be deleted", min_value=1, format="%d")
+    if st.button("Delete Product"):
         response = requests.delete(f"http://backend:8000/products/{delete_id}")
         show_response_message(response)
 
 # Atualizar Produto
-with st.expander("Atualizar Produto"):
+with st.expander("Update Product"):
     with st.form("update_product"):
-        update_id = st.number_input("ID do Produto", min_value=1, format="%d")
-        new_name = st.text_input("Novo Nome do Produto")
-        new_description = st.text_area("Nova Descrição do Produto")
+        update_id = st.number_input("Product ID", min_value=1, format="%d")
+        new_name = st.text_input("New Product Name")
+        new_description = st.text_area("New Product Description")
         new_price = st.number_input(
-            "Novo Preço",
+            "New Price",
             min_value=0.01,
             format="%f",
         )
         new_categoria = st.selectbox(
-            "Nova Categoria",
-            ["Eletrônico", "Eletrodoméstico", "Móveis", "Roupas", "Calçados"],
+            "New Category",
+            ["Eletronic", "Eletrodomestic", "Forniture", "Clothes", "Shoes"],
         )
-        new_email = st.text_input("Novo Email do Fornecedor")
+        new_email = st.text_input("New Vendor Email")
 
-        update_button = st.form_submit_button("Atualizar Produto")
+        update_button = st.form_submit_button("Update Product")
 
         if update_button:
             update_data = {}
@@ -139,9 +139,9 @@ with st.expander("Atualizar Produto"):
             if new_price > 0:
                 update_data["price"] = new_price
             if new_email:
-                update_data["email_fornecedor"] = new_email
+                update_data["vendor_email"] = new_email
             if new_categoria:
-                update_data["categoria"] = new_categoria
+                update_data["category"] = new_categoria
 
             if update_data:
                 response = requests.put(
@@ -149,4 +149,4 @@ with st.expander("Atualizar Produto"):
                 )
                 show_response_message(response)
             else:
-                st.error("Nenhuma informação fornecida para atualização")
+                st.error("No product information was informed to be updated")
